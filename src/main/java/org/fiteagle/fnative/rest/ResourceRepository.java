fe6dd560c62fba739387ac9edb60e86b904dd802
AlexanderWillner <alexander.willner@tu-berlin.de>
2014-03-04 11:33:11 +0100

    added async example (not finished)

--- src/main/java/org/fiteagle/fnative/rest/ResourceRepository.java
+++ src/main/java/org/fiteagle/fnative/rest/ResourceRepository.java
@@ -3,6 +3,14 @@ package org.fiteagle.fnative.rest;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.annotation.Resource;
+import javax.inject.Inject;
+import javax.jms.JMSConsumer;
+import javax.jms.JMSContext;
+import javax.jms.JMSException;
+import javax.jms.JMSProducer;
+import javax.jms.TextMessage;
+import javax.jms.Topic;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
@@ -13,33 +21,55 @@ import javax.ws.rs.Produces;
 import org.fiteagle.api.core.IResourceRepository;
 import org.fiteagle.api.core.IResourceRepository.Serialization;
 
-
 @Path("/repo")
-public class ResourceRepository{
-  
-  private static Logger log = Logger.getLogger(ResourceRepository.class.toString());
-  
-  private IResourceRepository repo;
-  
-  public ResourceRepository() throws NamingException{
-    final Context context = new InitialContext();
-    repo = (IResourceRepository) context.lookup("java:global/core-resourcerepository/ResourceRepository");
-  }
-  
-  @GET
-  @Path("/resources.rdf")
-  @Produces("application/rdf+xml")
-  public String listResourcesXML() {
-	  log.log(Level.INFO, "Getting resources as RDF...");
-      return repo.listResources(Serialization.XML);
-  }
+public class ResourceRepository {
 
-  @GET
-  @Path("/resources.ttl")
-  @Produces("text/turtle")
-  public String listResourcesTTL() {
-	  log.log(Level.INFO, "Getting resources as TTL...");
-      return repo.listResources(Serialization.TTL);
-  }
+	private static Logger log = Logger.getLogger(ResourceRepository.class
+			.toString());
+	private IResourceRepository repo;
+	@Inject
+	private JMSContext context;
+	@Resource(mappedName = "java:/topic/core")
+	private Topic topic;
+
+	public ResourceRepository() throws NamingException {
+		final Context context = new InitialContext();
+		repo = (IResourceRepository) context
+				.lookup("java:global/core-resourcerepository/ResourceRepositoryEJB");
+	}
+
+	@GET
+	@Path("/resources.rdf")
+	@Produces("application/rdf+xml")
+	public String listResourcesXML() {
+		log.log(Level.INFO, "Getting resources as RDF...");
+		return repo.listResources(Serialization.XML);
+	}
+
+	@GET
+	@Path("/resources.ttl")
+	@Produces("text/turtle")
+	public String listResourcesTTL() {
+		log.log(Level.INFO, "Getting resources as TTL...");
+		return repo.listResources(Serialization.TTL);
+	}
+
+	@GET
+	@Path("/async/resources.rdf")
+	@Produces("application/rdf+xml")
+	public String listResourcesXMLviaMDB() throws JMSException {
+		String text = "timeout";
+		
+		log.log(Level.INFO, "Getting resources as RDF via MDB...");
+		JMSProducer producer = context.createProducer();
+		producer.send(topic, "test");
+		JMSConsumer consumer = context.createConsumer(topic);
+		
+		TextMessage result = (TextMessage) consumer.receive(3000);
+		if (null != result)
+			text = result.getText();
+		
+		return text;
+	}
 
 }
