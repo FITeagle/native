f8d144fbff062455b25dd0c5aa8f5efb90d37afe
Alex-O. <wpinf05.ortlieb@gmail.com>
2015-07-16 17:05:28 +0200

    Timer class now implemented

--- src/main/java/start/StartUp.java
+++ src/main/java/start/StartUp.java
@@ -3,12 +3,14 @@ package start;
 import info.openmultinet.ontology.exceptions.InvalidModelException;
 import info.openmultinet.ontology.vocabulary.Omn_resource;
 
+import java.util.concurrent.Callable;
+
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
 
-import org.apache.jena.atlas.logging.Log;
+import org.fiteagle.api.core.TimerHelper;
 import org.fiteagle.core.tripletStoreAccessor.TripletStoreAccessor;
 import org.fiteagle.core.tripletStoreAccessor.TripletStoreAccessor.ResourceRepositoryException;
 
@@ -19,41 +21,64 @@ import com.hp.hpl.jena.rdf.model.Resource;
 @Startup
 @Singleton
 public class StartUp {
-	
-	private Model defaultModel;	
 
+	private Model defaultModel;
 
 	@PostConstruct
-    public void addNativeApi(){
-    	Model model = ModelFactory.createDefaultModel();
-    Resource resource = model.createResource("http://localhost/resource/Native");
-    resource.addProperty(Omn_resource.hasInterface, "/native/api/lodlive");
-    resource.addProperty(Omn_resource.hasInterface, "/native/api/resources");
-    resource.addProperty(Omn_resource.hasInterface, "/native/api/resources/testbed");
-    resource.addProperty(Omn_resource.hasInterface, "/native/api/resources/${resourceName}");
-    resource.addProperty(Omn_resource.hasInterface, "/native/api/resources/${adapterName}/instances");
-    try {
-		TripletStoreAccessor.addResource(resource);
-		defaultModel = model;
-	} catch (ResourceRepositoryException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+	public void addNativeApi() {
+		setDefaultModel();
+		TimerHelper timer = new TimerHelper(new NativeAPI());
 	}
-    }
-	
-	
+
 	@PreDestroy
 	public void deleteNativeApi() {
-		try{	
-	    TripletStoreAccessor.deleteModel(defaultModel);
-		} catch (ResourceRepositoryException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InvalidModelException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		TimerHelper timer = new TimerHelper(new DeleteNativeAPI());
+	}
+
+	private Model setDefaultModel() {
+		Model model = ModelFactory.createDefaultModel();
+		Resource resource = model
+				.createResource("http://localhost/resource/Native");
+		resource.addProperty(Omn_resource.hasInterface, "/native/api/lodlive");
+		resource.addProperty(Omn_resource.hasInterface, "/native/api/resources");
+		resource.addProperty(Omn_resource.hasInterface,
+				"/native/api/resources/testbed");
+		resource.addProperty(Omn_resource.hasInterface,
+				"/native/api/resources/${resourceName}");
+		resource.addProperty(Omn_resource.hasInterface,
+				"/native/api/resources/${adapterName}/instances");
+		defaultModel = model;
+
+		return model;
+	}
+
+	class DeleteNativeAPI implements Callable<Void> {
+
+		@Override
+		public Void call() throws ResourceRepositoryException,
+				InvalidModelException {
+			if (defaultModel == null) {
+				TripletStoreAccessor.deleteModel(setDefaultModel());
+			} else {
+				TripletStoreAccessor.deleteModel(defaultModel);
+			}
+			return null;
 		}
-		
-		
+	}
+
+	class NativeAPI implements Callable<Void> {
+
+		@Override
+		public Void call() throws ResourceRepositoryException {
+			if (defaultModel == null) {
+				setDefaultModel();
+				TripletStoreAccessor.addResource(setDefaultModel().getResource(
+						"http://localhost/resource/Native"));
+			} else {
+				TripletStoreAccessor.addResource(defaultModel
+						.getResource("http://localhost/resource/Native"));
+			}
+			return null;
+		}
 	}
 }
